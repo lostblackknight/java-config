e0f38e912e50e9eb692e19dceae82ae9252d4cc4
lostblackknight <chensixiang12345@gmail.com>
2021-06-03 13:10:29 +0800

    add mybatis, log4j2 config

---
+++ src/main/java/io/github/lostbalckknight/mybatis/MybatisUtils.java
@@ -0,0 +1,29 @@
+package io.github.lostbalckknight.mybatis;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author chensixiang chensixiang1234@gmail.com
+ */
+public class MybatisUtils {
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        try {
+            InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static SqlSession getSqlSession() {
+        return sqlSessionFactory.openSession();
+    }
+}
